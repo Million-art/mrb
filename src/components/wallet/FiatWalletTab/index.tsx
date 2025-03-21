1f3bbda1149b7feda0cadf9da374a4c7e56d71ee
million-art <millionmulugeta09@gmail.com>
2025-03-21 20:44:27 +0300

    fix css

--- src/components/wallet/FiatWalletTab/index.tsx
+++ src/components/wallet/FiatWalletTab/index.tsx
@@ -49,7 +49,7 @@ const FiatWalletTab = () => {
       <Card className="rounded-lg shadow-md min-w-full scrollbar-hidden">
         <div className="mb-2">
           <p className="text-gray-300">Your Balance</p>
-          <h1 className="text-2xl font-bold">{realBalance.toFixed(2)} USDC</h1> 
+          <h1 className="text-3xl font-bold">{realBalance.toFixed(2)} USDC</h1> 
           <div className="bg-gray-900 inline-block rounded-md">
             <p className="font-bold"></p>
           </div>
