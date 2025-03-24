a135a334b32bf59a0b6610f5745b88275c47a604
million-art <millionmulugeta09@gmail.com>
2025-03-24 20:44:18 +0300

    deposit

--- src/interface/Ambassador.ts
+++ src/interface/Ambassador.ts
@@ -1,23 +1,24 @@
 
-export interface PaymentMethod {
-    id: string
-    bankName: string
-    accountNumber: string
 
-  }
+export interface PaymentMethod {
+  type: string;
+  details: Record<string, any>;
+}
 
 export interface Ambassador {
-    id: string;
-    firstName?: string;
-    lastName?: string;
-    tgUsername?: string;
-    email?: string;
-    phone?: string;
-    country?: string;
-    photoUrl?: string; // Optional field (URL or base64 string)
-    paymentMethods?: PaymentMethod[]; // Array of payment methods
-    createdAt?: Date;
-    role: string; //  "ambassador", "admin"
-    kyc?: string; // Optional field ( "pending", "approved", "rejected")
-  }
-  
+  id: string;
+  firstName: string;
+  lastName: string;
+  tgUsername: string;
+  email: string;
+  phone: string;
+  address?: string;
+  idFront?: string;
+  idBack?: string;
+  country: string;
+  photoUrl?: string;
+  paymentMethods: PaymentMethod[];
+  createdAt: Date;
+  role: string;
+  kyc?: string;
+}
